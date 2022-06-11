1e93e7da05ac4fd247fef64e2aa22a47abe3ee51
PC <nrkrks@gmail.com>
2022-06-11 23:24:18 +0300

    sa

---
+++ src/main/kotlin/1.kt
@@ -0,0 +1,49 @@
+/*fun main() {
+
+   /* val schoolNumber = 126.toByte()
+    val convertedValue: Short = schoolNumber.toShort()
+
+    println("School Number  " + schoolNumber)
+    println("converted Value    " + convertedValue)
+*/
+    val tc = 39020054700
+    val convertedtc = tc.toByte()
+    println("tc " + tc)
+    println("donusmus tc " + convertedtc)
+}
+*/
+ /*
+ fun main() {
+
+
+    val charNumber: Char = '8'
+
+}
+
+
+fun main() {
+
+    val isTeacher: Boolean = true
+ //if (isTeacher)             //öğretmen mi kontrolu
+  //   if (!isTeacher)        //öğretmen değil mi kontrolü
+
+    val studentNumbers = arrayOf(13,21,33,44,55,)
+
+
+    */
+
+fun main()
+{
+    val studentNumbers = arrayOf(13,21,33,44,55,)
+    val studentNames = arrayOf("ahmet", "ayşe" , "nazlı")
+    val firsCharOfNames = arrayOf('a' , 'a' , 'n')
+    val mixedArray = arrayOf<Any>(13 , " ahmet" , 'n')
+    val arrayOfNulls = arrayOfNulls<String>(22 )
+val charArray = CharArray (4)       //toplam 4 tane index atandı
+charArray [1] = 't'   //birinci indexe t yi atadık yani. ama index 0 dan başlar.
+
+
+
+
+
+}
